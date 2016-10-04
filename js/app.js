f2d7bbc104743e981e15fe9a2b9c12a63b8f26fa
Vincent Yan <vincentyan1@gmail.com>
2016-10-04 00:19:04 -0700

    Added random quote function and twitter event listener

--- js/app.js
+++ js/app.js
@@ -1,30 +1,58 @@
-var quotesArray = [
-		"For over a thousand generations, the Jedi were the guardians of peace and justice in the Old Republic.<footer class=\"text-right\" id=\"author\">Obi-Wan Kenobi</footer>",
-		"Fear is the path to the dark side. Fear leads to anger. Anger leads to hate. Hate leads to suffering.<footer class=\"text-right\" id=\"author\">Yoda</footer>",
-		"So this is how liberty dies...with thunderous applause.<footer class=\"text-right\" id=\"author\">Padme Amidala</footer>",
-		"It's a TRAP!!!<footer class=\"text-right\" id=\"author\">Admiral Ackbar</footer>",
-		"I'm rather embarrassed, General Solo. But it appears you are to be the main course at a banquet in my honor.<footer class=\"text-right\" id=\"author\">C-3PO</footer>",
-		"You can't stop change any more than you can stop the suns from setting.<footer class=\"text-right\" id=\"author\">Shmi Skywalker</footer>",
-		"Don't be too proud of this technological terror you have constructed. The ability to destroy a planet is insignificant next to the power of the Force.<footer class=\"text-right\" id=\"author\">Darth Vader</footer>",
-		"Why, you stuck up, half-witted, scruffy-looking Nerf herder!<footer class=\"text-right\" id=\"author\">Princess Leia</footer>",
-		"Hokey religions and ancient weapons are no match for a good blaster at your side, kid.<footer class=\"text-right\" id=\"author\">Han Solo</footer>",
-		"You failed, Your Highness. I am a Jedi, like my father before me.<footer class=\"text-right\" id=\"author\">Luke Skywalker</footer>", 
-		"I'm looking forward to completing your training. In time you will call *me* master.<footer class=\"text-right\" id=\"author\">Emperor Palpatine</footer>"
-];
-
+var quotesBank = [{
+    "quote": "For over a thousand generations, the Jedi were the guardians of peace and justice in the Old Republic.",
+    "character": "Obi-Wan Kenobi"
+}, {
+    "quote": "Try not. Do, or do not. There is no try.",
+    "character": "Yoda"
+}, {
+    "quote": "So this is how liberty dies...with thunderous applause.",
+    "character": "Padme Amidala"
+}, {
+    "quote": "It's a TRAP!!!",
+    "character": "Admiral Ackbar"
+}, {
+    "quote": "Sir, the possibility of successfully navigating an asteroid field is approximately 3,720 to 1!",
+    "character": "C-3PO"
+}, {
+    "quote": "You can't stop change any more than you can stop the suns from setting",
+    "character": "Shmi Skywalker"
+}, {
+    "quote": "I find your lack of faith disturbing.",
+    "character": "Darth Vader"
+}, {
+    "quote": "Why, you stuck up, half-witted, scruffy-looking Nerf herder!",
+    "character": "Princess Leia"
+}, {
+    "quote": "Hokey religions and ancient weapons are no match for a good blaster at your side, kid.",
+    "character": "Han Solo"
+}, {
+    "quote": "You failed, Your Highness. I am a Jedi, like my father before me.",
+    "character": "Luke Skywalker"
+}, {
+    "quote": "I'm looking forward to completing your training. In time you will call *me* master.",
+    "character": "Emperor Palpatine"
+}];
 
 var tweetButton = document.getElementById("tweet");
 var quoteButton = document.getElementById("new-quote");
 
 getQuote();
 
-function getQuote(){
+function getQuote() {
 	var newQuote = document.getElementById("quote");
-	var randomQuote = Math.floor(Math.random() * quotesArray.length);
-	console.log(randomQuote);
-	return newQuote.innerHTML = quotesArray[randomQuote];
+	var author = document.getElementById("author");
+  var randomQuote = Math.floor(Math.random() * quotesBank.length);
+
+  newQuote.textContent = quotesBank[randomQuote].quote;
+  author.textContent = quotesBank[randomQuote].character;
 }
 
-quoteButton.addEventListener("click", function(){
-	 getQuote();
-}) 
+quoteButton.addEventListener("click", function() {
+  getQuote();
+});
+
+tweetButton.addEventListener("click", function(){
+	var currentQuote = document.getElementById("quote");
+	var currentAuthor = document.getElementById("author");
+	window.open("https://twitter.com/intent/tweet?text=" + "\"" + currentQuote.textContent + "\"" + " - " + currentAuthor.textContent);
+});
