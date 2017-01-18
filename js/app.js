e2ea2626ea819f9851b6a49f416f72b8606e0026
Vincent Yan <vincentyan1@gmail.com>
2017-01-17 16:46:07 -0800

    Added small styling changes

--- js/app.js
+++ js/app.js
@@ -1,42 +1,42 @@
 var quotesBank = [{
-    "quote": "For over a thousand generations, the Jedi were the guardians of peace and justice in the Old Republic.",
-    "character": "Obi-Wan Kenobi"
+	"quote": "For over a thousand generations, the Jedi were the guardians of peace and justice in the Old Republic.",
+	"character": "Obi-Wan Kenobi"
 }, {
-    "quote": "Try not. Do, or do not. There is no try.",
-    "character": "Yoda"
+	"quote": "Try not. Do, or do not. There is no try.",
+	"character": "Yoda"
 }, {
-    "quote": "So this is how liberty dies...with thunderous applause.",
-    "character": "Padme Amidala"
+	"quote": "So this is how liberty dies...with thunderous applause.",
+	"character": "Padme Amidala"
 }, {
-    "quote": "It's a TRAP!!!",
-    "character": "Admiral Ackbar"
+	"quote": "It's a TRAP!!!",
+	"character": "Admiral Ackbar"
 }, {
-    "quote": "You will remove these restraints and leave this cell with the door open.",
-    "character": "Rey"
+	"quote": "You will remove these restraints and leave this cell with the door open.",
+	"character": "Rey"
 }, {
-    "quote": "Sir, the possibility of successfully navigating an asteroid field is approximately 3,720 to 1!",
-    "character": "C-3PO"
+	"quote": "Sir, the possibility of successfully navigating an asteroid field is approximately 3,720 to 1!",
+	"character": "C-3PO"
 }, {
-    "quote": "You can't stop change any more than you can stop the suns from setting",
-    "character": "Shmi Skywalker"
+	"quote": "You can't stop change any more than you can stop the suns from setting",
+	"character": "Shmi Skywalker"
 }, {
-    "quote": "I find your lack of faith disturbing.",
-    "character": "Darth Vader"
+	"quote": "I find your lack of faith disturbing.",
+	"character": "Darth Vader"
 }, {
-    "quote": "Nothing will stand in our way. I will finish what you started.",
-    "character": "Kylo Ren"
+	"quote": "Nothing will stand in our way. I will finish what you started.",
+	"character": "Kylo Ren"
 }, {
-    "quote": "Why, you stuck up, half-witted, scruffy-looking Nerf herder!",
-    "character": "Princess Leia"
+	"quote": "Why, you stuck up, half-witted, scruffy-looking Nerf herder!",
+	"character": "Princess Leia"
 }, {
-    "quote": "Hokey religions and ancient weapons are no match for a good blaster at your side, kid.",
-    "character": "Han Solo"
+	"quote": "Hokey religions and ancient weapons are no match for a good blaster at your side, kid.",
+	"character": "Han Solo"
 }, {
-    "quote": "You failed, Your Highness. I am a Jedi, like my father before me.",
-    "character": "Luke Skywalker"
+	"quote": "You failed, Your Highness. I am a Jedi, like my father before me.",
+	"character": "Luke Skywalker"
 }, {
-    "quote": "I'm looking forward to completing your training. In time you will call *me* master.",
-    "character": "Emperor Palpatine"
+	"quote": "I'm looking forward to completing your training. In time you will call *me* master.",
+	"character": "Emperor Palpatine"
 }];
 
 var tweetButton = document.getElementById("tweet");
@@ -45,16 +45,16 @@ var quoteButton = document.getElementById("new-quote");
 getQuote();
 
 function getQuote() {
-  var newQuote = document.getElementById("quote");
-  var author = document.getElementById("author");
-  var randomQuote = Math.floor(Math.random() * quotesBank.length);
+	var newQuote = document.getElementById("quote");
+	var author = document.getElementById("author");
+	var randomQuote = Math.floor(Math.random() * quotesBank.length);
 
-  newQuote.textContent = quotesBank[randomQuote].quote;
-  author.textContent = quotesBank[randomQuote].character;
+	newQuote.textContent = quotesBank[randomQuote].quote;
+	author.textContent = quotesBank[randomQuote].character;
 }
 
 quoteButton.addEventListener("click", function() {
-  getQuote();
+  	getQuote();
 });
 
 tweetButton.addEventListener("click", function(){
